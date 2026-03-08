0ed5f338053ad62fd93c4a379c9467910c763545
MoosaTeam <mrcrocodile720@gmail.com>
2026-03-08 15:47:32 +0500

    data file v2

--- generate_data.py
+++ generate_data.py
@@ -8,13 +8,13 @@ filepath = "data/unseen_climate_data.csv"
 
 countries = ["Senegal", "Canada", "Japan", "Brazil", "Germany"]
 
-print(f"Generating 50,000 rows of test data at {filepath}...")
+print(f"Generating rows of test data at {filepath}...")
 with open(filepath, "w", newline="", encoding="utf-8") as file:
     writer = csv.writer(file)
     # These headers MUST match your config.json source_names exactly
     writer.writerow(["Country Name", "Year", "Average_Temperature_C", "Ignore_This_Column"])
     
-    for i in range(50000):
+    for i in range(100):
         country = random.choice(countries)
         year = 2026 - (i % 50)
         temp = round(random.uniform(-10.0, 45.0), 2)
